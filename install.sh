d680739e7cb08ec3034abebad50b6c220b98fd30
Zephranoid <zephranoid@zephranoid.com>
2018-03-30 19:26:33 +1100

    Moved files that don't need to be in each theme into constants folder

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 dir=$(pwd)/$1
 constants=$(pwd)/constants
 echo $dir
-ln -sf $dir/i3/config ~/.config/i3/config
+ln -sf $constants/i3/config ~/.config/i3/config
 ln -sf $dir/polybar/config ~/.config/polybar/config
-ln -sf $dir/compton/compton.conf ~/.config/compton/compton.conf
+ln -sf $constants/compton/compton.conf ~/.config/compton/compton.conf
 ln -sf $dir/dunst/dunstrc ~/.config/dunst/dunstrc
-ln -sf $dir/qutebrowser/config.py ~/.config/qutebrowser/config.py
-ln -sf $dir/rofi/config ~/.config/rofi/config
+ln -sf $constants/qutebrowser/config.py ~/.config/qutebrowser/config.py
+ln -sf $constants/rofi/config ~/.config/rofi/config
 ln -sf $dir/termite/config ~/.config/termite/config
 ln -sf $dir/vim/theme.vim ~/.vim/colors/theme.vim
 ln -sf $constants/vimrc ~/.vimrc
