6e2278fc66b559a5447f3d5ad2dd23fd0a89bfdf
Zephranoid <zephranoid@zephranoid.com>
2018-05-17 15:33:43 +1000

    Changed folder layout

--- install.sh
+++ install.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-dir=$(pwd)/$1
-constants=$(pwd)/constants
-echo $dir
-ln -sf $constants/i3/config ~/.config/i3/config
+dir=$(pwd)
+ln -sf $dir/i3/config ~/.config/i3/config
 ln -sf $dir/polybar/config ~/.config/polybar/config
-ln -sf $constants/compton/compton.conf ~/.config/compton/compton.conf
+ln -sf $dir/compton/compton.conf ~/.config/compton/compton.conf
 ln -sf $dir/dunst/dunstrc ~/.config/dunst/dunstrc
-ln -sf $constants/qutebrowser/config.py ~/.config/qutebrowser/config.py
-ln -sf $constants/rofi/config ~/.config/rofi/config
+ln -sf $dir/qutebrowser/config.py ~/.config/qutebrowser/config.py
+ln -sf $dir/rofi/config ~/.config/rofi/config
 ln -sf $dir/termite/config ~/.config/termite/config
 ln -sf $dir/vim/theme.vim ~/.vim/colors/theme.vim
-ln -sf $constants/vimrc ~/.vimrc
-ln -sf $constants/bashrc ~/.bashrc
+ln -sf $dir/vimrc ~/.vimrc
+ln -sf $dir/bashrc ~/.bashrc
 ln -sf $dir/background.jpg ~/background.jpg
 ln -sf $dir/rofi/custom.rasi ~/.config/rofi/custom.rasi
--- vimrc
+++ vimrc
@@ -1,5 +1,11 @@
 set nocompatible
 filetype off
+"Vundle Config
+set rtp+=~/.vim/bundle/Vundle.vim
+call vundle#begin()
+Plugin 'VundleVim/Vundle.vim'
+Plugin 'Valloric/YouCompleteMe'
+call vundle#end()
 
 filetype plugin indent on
 syntax on
@@ -39,3 +45,5 @@ nnoremap <F12> :call IndentFile()<cr>
 noremap <F10> :echo "hi<" . synIDattr(synID(line("."),col("."),1),"name") . '> trans<'
 \ . synIDattr(synID(line("."),col("."),0),"name") . "> lo<"
 \ . synIDattr(synIDtrans(synID(line("."),col("."),1)),"name") . ">"<CR> 
+
+let g:ycm_global_ycm_extra_conf = '~/.vim/.ycm_extra_conf.py'
